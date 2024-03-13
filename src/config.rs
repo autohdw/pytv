2df672276f803b5eb4615005fafb8aea18e3e0ce
Teddy van Jerry <me@teddy-van-jerry.org>
2024-03-14 01:21:36 +0800

    Update Python Line Parsing

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,9 @@ use regex::Regex;
 pub struct Config {
     pub magic_comment_str: String,
     pub template_re: Regex,
+    pub run_python: bool,
+    pub delete_python: bool,
+    pub tab_size: u32,
 }
 
 #[derive(Debug)]
@@ -16,7 +19,13 @@ pub struct FileOptions {
 
 impl Default for Config {
     fn default() -> Self {
-        Self::new("!".to_string(), Self::default_template_re())
+        Self::new(
+            "!".to_string(),
+            Self::default_template_re(),
+            false,
+            false,
+            4,
+        )
     }
 }
 
@@ -24,22 +33,50 @@ impl Default for Config {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// input file
+    /// Input file
     #[arg(index = 1, value_name = "FILE")]
     input: String,
-    /// output file
+    /// Output file
     #[arg(short, long)]
     output: Option<String>,
+    /// Run python (keep Python script)
+    #[arg(
+        short = 'r',
+        long = "run-py",
+        conflicts_with = "run_python_del",
+        default_value = "false"
+    )]
+    run_python: bool,
+    /// Run python (delete Python script)
+    #[arg(
+        short = 'R',
+        long = "run-py-del",
+        conflicts_with = "run_python",
+        default_value = "false"
+    )]
+    run_python_del: bool,
+    /// Tab size
+    #[arg(short, long, default_value = "4", value_name = "INT")]
+    tab_size: u32,
     /// Magic comment string (after "//")
-    #[arg(short, long, default_value = "!")]
+    #[arg(short, long, default_value = "!", value_name = "STRING")]
     magic: String,
 }
 
 impl Config {
-    pub fn new(magic_comment_str: String, template_re: Regex) -> Config {
+    pub fn new(
+        magic_comment_str: String,
+        template_re: Regex,
+        run_python: bool,
+        delete_python: bool,
+        tab_size: u32,
+    ) -> Config {
         Config {
             magic_comment_str,
             template_re,
+            run_python,
+            delete_python,
+            tab_size,
         }
     }
 
@@ -47,7 +84,13 @@ impl Config {
     pub fn from_args() -> (Config, FileOptions) {
         let args = Args::parse();
         (
-            Self::new(args.magic, Self::default_template_re()),
+            Self::new(
+                args.magic,
+                Self::default_template_re(),
+                args.run_python || args.run_python_del,
+                args.run_python_del && !args.run_python,
+                args.tab_size,
+            ),
             FileOptions {
                 input: args.input,
                 output: args.output,
